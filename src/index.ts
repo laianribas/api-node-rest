89169513023816613a82ed66dc0234bd18b2083b
laianribas <laian67a@gmail.com>
2022-02-18 21:00:05 -0300

    Adicionando o typescript e incrementando a forma de desenvolver

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Executei')
+import express, { Request, Response, NextFunction } from 'express';
+const app = express();
+app.listen(3003, () => {
+  console.log('listening on port 3003');
+})
+app.get('/status', (request: Request, response: Response, next: NextFunction) => {
+  response.status(200).send({ foo: 'bau' });
+})
